bb4dd4197271d69d8862454488834a9dca256742
OksankaLukianovets <lukianovets.oksana@gmail.com>
2023-04-10 11:03:58 -0400

    branch2 - commit 4 - total 7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
         test_2.print();
-        test_2.print();
-        test_2.print();
+        System.out.println("Everything is working fine");
 
     }
 }
